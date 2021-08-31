b7967280eeaa61cb9804291b03c1a7d4cb7c72ff
ValeriCodeU <valeri.yanev@icloud.com>
2021-08-31 16:13:11 +0300

    Added Programming Basics course solutions

---
+++ Programming-Basic-CSharp/02.FirstStepsInCoding-Exercise/VacationBooksList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPages = int.Parse(Console.ReadLine());
+            double pagesPerHour = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            double allTime = numOfPages / pagesPerHour;
+            double hoursPerDay = allTime / days;
+
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
