b98be51948ce8bffb8e54351b938c15b16f69d5b
ValeriCodeU <valeri.yanev@icloud.com>
2021-08-31 16:28:11 +0300

    Added Programming Basics course solutions

---
+++ Programming-Basics-CSharp/04.ConditionalStatements-Exercise/Scholarship/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double income = double.Parse(Console.ReadLine());
+            double averageSuccess = double.Parse(Console.ReadLine());
+            double minSalary = double.Parse(Console.ReadLine());
+
+            
+            double socialScholarship = minSalary * 0.35;
+            double excellentScholarship = averageSuccess * 25;
+
+            if (income < minSalary && averageSuccess >= 5.50)
+            {
+                if (socialScholarship > excellentScholarship)
+                {
+                    Console.WriteLine($"You get a Social scholarship {Math.Floor(socialScholarship)} BGN");
+                }
+                else
+                {
+                    Console.WriteLine($"You get a scholarship for excellent results {Math.Floor(excellentScholarship)} BGN");
+                }
+            }
+
+            else if (income < minSalary && averageSuccess > 4.50)
+            {
+                Console.WriteLine($"You get a Social scholarship {Math.Floor(socialScholarship)} BGN");
+
+            }
+            else if (averageSuccess >= 5.50)
+            {
+                Console.WriteLine($"You get a scholarship for excellent results {Math.Floor(excellentScholarship)} BGN");
+            }
+            else
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+    }
+        }
+}
---
+++ Programming-Basics-CSharp/04.ConditionalStatements-Exercise/WorldSwimmingRecord/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distanceMeters = double.Parse(Console.ReadLine());
+            double timeSeconds = double.Parse(Console.ReadLine());
+
+            double allTime = distanceMeters * timeSeconds;
+            double delayTime = Math.Floor(distanceMeters / 15);
+
+            allTime = allTime + delayTime * 12.5;
+
+            if (allTime < record)
+            {                
+                Console.WriteLine($"Yes, he succeeded! The new world record is {allTime:f2} seconds.");
+            }
+            //else 
+            //{
+            //    double lackTime = allTime - record;
+            //    Console.WriteLine($"No, he failed! He was {lackTime:f2} seconds slower.");
+            //}
+            else if (allTime >= record)
+            {
+                double lackTime = allTime - record;
+                Console.WriteLine($"No, he failed! He was {lackTime:f2} seconds slower.");
+            }
+            
+            
+
+
+        }
+    }
+}
