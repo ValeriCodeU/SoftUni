c35c5caef602d751d855f1ea3afb283fb47084ba
ValeriCodeU <valeri.yanev@icloud.com>
2021-08-31 21:38:07 +0300

    Added Programming Basics course solutions

---
+++ Programming-Basics-CSharp/06.ConditionalStatementsAdvanced-Exercise/FishingBoat/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace FishingBoat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermen = int.Parse(Console.ReadLine());
+
+            double priceBoat = 0;
+
+
+            
+            if (fishermen <= 6)
+            {
+
+                switch (season)
+                {
+                    case "Spring":
+                        priceBoat = 3000 * 0.9;
+                        break;
+                    case "Summer":
+                        priceBoat = 4200 * 0.9;
+                        break;
+                    case "Autumn":
+                        priceBoat = 4200 * 0.9;
+                        break;
+                    case "Winter":
+                        priceBoat = 2600 * 0.9;
+                        break;
+                }
+            }
+
+            else if (fishermen >= 7 && fishermen <= 11)
+            {
+                switch (season)
+                {
+                    case "Spring":
+                        priceBoat = 3000 * 0.85;
+                        break;
+                    case "Summer":
+                        priceBoat = 4200 * 0.85;
+                        break;
+                    case "Autumn":
+                        priceBoat = 4200 * 0.85;
+                        break;
+                    case "Winter":
+                        priceBoat = 2600 * 0.85;
+                        break;
+                }
+            }
+            else if (fishermen >= 12)
+            {
+                switch (season)
+                {
+                    case "Spring":
+                        priceBoat = 3000 * 0.75;
+                        break;
+                    case "Summer":
+                        priceBoat = 4200 * 0.75;
+                        break;
+                    case "Autumn":
+                        priceBoat = 4200 * 0.75;
+                        break;
+                    case "Winter":
+                        priceBoat = 2600 * 0.75;
+                        break;
+                }
+            }
+
+            if (fishermen % 2 == 0 && season != "Autumn")
+            {
+                priceBoat -= priceBoat * 0.05;
+            }
+           
+            if (budget >= priceBoat)
+            {
+                Console.WriteLine($"Yes! You have {budget-priceBoat:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {priceBoat-budget:f2} leva.");
+            }
+            
+        }
+    }
+}
---
+++ Programming-Basics-CSharp/06.ConditionalStatementsAdvanced-Exercise/OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace OperationsBetweenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine());
+            string symbol = Console.ReadLine();
+
+            double result = 0;
+            string evenOdd = "";
+            
+            switch (symbol)
+            {
+                case "+":
+                    result = n1 + n2;
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    break;                  
+                case "*":
+                    result = n1 * n2;                   
+                    break;
+                case "/":                  
+                    
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {n1} by zero");
+                    }
+                    else
+                    {
+                        result = n1 / n2;
+                        Console.WriteLine($"{n1} {symbol} {n2} = {result:f2}");
+                    }
+                    break;
+                case "%":
+                    
+                    if (n2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {n1} by zero");
+                    }
+                    else
+                    {
+                        result = n1 % n2;
+                        Console.WriteLine($"{n1} {symbol} {n2} = {result}");
+                    }
+                    break;
+            }
+
+            if (symbol == "+" || symbol == "-" || symbol == "*")
+            {
+                if (result % 2 == 0)
+                {
+                    evenOdd = "even";
+
+                    Console.WriteLine($"{n1} {symbol} {n2} = {result} - {evenOdd}");
+                }
+                else
+                {
+                    evenOdd = "odd";
+                    Console.WriteLine($"{n1} {symbol} {n2} = {result} - {evenOdd}");
+                }
+                
+            }
+           
+
+
+
+           
+        }
+    }
+}
