43f1b2d65e00142f9378152603ce044c8762cc8a
ValeriCodeU <89798512+ValeriCodeU@users.noreply.github.com>
2021-09-02 13:54:43 +0300

    Edited Programming Basics course solutions

--- Programming-Basics-CSharp/05.1.WhileLoop-Lab/SumNumbers/Program.cs
+++ Programming-Basics-CSharp/05.1.WhileLoop-Lab/SumNumbers/Program.cs
@@ -7,26 +7,18 @@ namespace SumNumbers
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-
             int sumNumbers = 0;
-
 
             while (n >= sumNumbers)
             {
                 int currentNum = int.Parse(Console.ReadLine());
-                
-
                 sumNumbers += currentNum;
 
                 if (sumNumbers == n)
                 {
-                    break;
-                    
+                    break;                    
                 }
-
-
             }
-
             Console.WriteLine(sumNumbers);
         }
     }
