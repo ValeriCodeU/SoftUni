abcbc370002a4d1cbdc0483baf26f5e84b1a3f2f
ValeriCodeU <89798512+ValeriCodeU@users.noreply.github.com>
2021-09-02 13:51:13 +0300

    Edited Programming Basics course solutions

--- Programming-Basics-CSharp/05.1.WhileLoop-Lab/Password/Program.cs
+++ Programming-Basics-CSharp/05.1.WhileLoop-Lab/Password/Program.cs
@@ -8,15 +8,12 @@ namespace Password
         {
             string name = Console.ReadLine();
             string pasword = Console.ReadLine();
-
             string inputPassword = Console.ReadLine();
 
             while (pasword != inputPassword)
             {
                 inputPassword = Console.ReadLine();
-                
             }
-
             Console.WriteLine($"Welcome {name}!");
         }
     }
