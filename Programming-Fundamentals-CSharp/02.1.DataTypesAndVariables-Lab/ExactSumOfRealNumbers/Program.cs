4f71d897dae5cfaaa7673340a06fc0bb3474e694
ValeriCodeU <valeri.yanev@icloud.com>
2021-09-08 23:38:50 +0300

    Added Programming Fundamentals course solutions

---
+++ Programming-Fundamentals-CSharp/02.1.DataTypesAndVariables-Lab/ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            decimal sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                decimal exactNum = decimal.Parse(Console.ReadLine());
+
+                sum += exactNum;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
