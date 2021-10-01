21873a964108f0bd17341952590676f4785c71b6
ValeriCodeU <valeri.yanev@icloud.com>
2021-10-01 23:27:29 +0300

    Added Programming Fundamentals course solutions

---
+++ Programming-Fundamentals-CSharp/06.1.ObjectsAndClasses-Lab/ObjectsAndClasses-Lab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split().ToList();
+            Random random = new Random();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                int swapPosition = random.Next(words.Count);
+                string temp = words[i];
+                words[i] = words[swapPosition];
+                words[swapPosition] = temp;
+
+            }
+
+            Console.WriteLine(String.Join("\n", words));
+        }
+    }
+}
