c0d4b51cee45612707d9ed2a7827d5ea799bedf7
ValeriCodeU <valeri.yanev@icloud.com>
2021-10-13 22:37:31 +0300

    Added Programming Fundamentals course solutions

---
+++ Programming-Fundamentals-CSharp/07.2.AssociativeArrays-Exercise/AMinerTask/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            string command = Console.ReadLine();
+            int counter = 0;
+            string key = "";
+
+            while (command != "stop")
+            {               
+                counter++;
+
+                if (counter % 2 != 0)
+                {
+                    key = command;
+                    if (!dictionary.ContainsKey(key))
+                    {
+                        dictionary.Add(key, 0);
+                    }
+                    else
+                    {
+                        command = Console.ReadLine();
+                        continue;
+                    }
+                }                
+                else if (counter % 2 == 0)
+                {
+                    dictionary[key] += int.Parse(command);
+                }
+
+                command = Console.ReadLine();
+
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ Programming-Fundamentals-CSharp/07.2.AssociativeArrays-Exercise/ForceBook/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ForceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+
+            while (command != "Lumpawaroo")
+            {
+                if (command.Contains("|"))
+                {
+                    string[] commandAdd = command.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                    string forceSide = commandAdd[0];
+                    string forceUser = commandAdd[1];
+
+                    if (!dictionary.ContainsKey(forceSide))
+                    {
+                        dictionary.Add(forceSide, new List<string>());
+                    }                    
+
+                    if (!dictionary.Any(x => x.Value.Contains(forceUser)))
+                    {
+                        dictionary[forceSide].Add(forceUser);
+                    }
+                }
+                else if (command.Contains("->"))
+                {
+                    string[] commandEdit = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    string forceUser = commandEdit[0];
+                    string forceSide = commandEdit[1];
+
+                    if (!dictionary.ContainsKey(forceSide))
+                    {
+                        dictionary.Add(forceSide, new List<string>());
+                    }
+
+                    if (!dictionary.Any(x => x.Value.Contains(forceUser)))
+                    {                        
+                        dictionary[forceSide].Add(forceUser);
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                    }
+                    else
+                    {
+                        dictionary = dictionary.Where(v => ! v.Value.Contains(forceUser)).ToDictionary(x => x.Key, x => x.Value);
+                        dictionary[forceSide].Add(forceUser);
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            var final = dictionary.Where(v => v.Value.Count > 0).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in final.OrderByDescending(v => v.Value.Count).ThenBy(k => k.Key))
+            {
+                Console.WriteLine($"Side: {item.Key}, Members: {item.Value.Count}");
+                Console.Write("! ");
+                Console.WriteLine(String.Join("\n! ", item.Value.OrderBy(x => x)));
+            }            
+        }
+    }
+}
