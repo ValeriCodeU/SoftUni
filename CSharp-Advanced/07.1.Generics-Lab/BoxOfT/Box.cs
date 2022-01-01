ce7569e51328b9d46109cc618e39d87d3f078dc2
ValeriCodeU <valeri.yanev@icloud.com>
2022-01-01 19:55:45 +0200

    Added CSharp Advanced course solutions

---
+++ CSharp-Advanced/07.1.Generics-Lab/BoxOfT/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> boxList = new List<T>();
+
+        public int Count => boxList.Count;
+
+        public void Add (T element)
+        {
+            boxList.Add(element);
+        }
+
+        public T Remove()
+        {
+            var element = boxList[boxList.Count - 1];
+            boxList.Remove(element);
+            return element;
+        }
+    }
+}
---
+++ CSharp-Advanced/07.1.Generics-Lab/GenericScale/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> scale1 = new EqualityScale<int>(10, 6);
+            EqualityScale<string> scale2 = new EqualityScale<string>("A", "A");            
+
+            Console.WriteLine(scale1.AreEqual());
+            Console.WriteLine(scale2.AreEqual());
+        }
+    }
+}
