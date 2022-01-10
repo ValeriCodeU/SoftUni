d076b79d5b8a68cd448c05b9f1dc6ab1f5d06186
ValeriCodeU <valeri.yanev@icloud.com>
2022-01-10 22:46:53 +0200

    Added CSharp OOP course solutions

---
+++ CSharp-OOP/03.1.InterfacesAndAbstraction-Lab/Shapes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IDrawable rectangle = new Rectangle(10, 5);
+
+            rectangle.Draw();
+
+            IDrawable circle = new Circle(10);
+
+            circle.Draw();           
+        }
+    }
+}
---
+++ CSharp-OOP/03.1.InterfacesAndAbstraction-Lab/Shapes/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width { get => width; set => width = value; }
+        public int Height { get => height; set => height = value; }
+
+        public void Draw()
+        {
+            for (int row = 0; row < Height; row++)
+            {
+                for (int col = 0; col < Width; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
