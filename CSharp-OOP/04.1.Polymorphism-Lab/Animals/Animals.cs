729ce85ad15607d7ab09ac08ed582fe5b1591581
ValeriCodeU <valeri.yanev@icloud.com>
2022-01-12 23:23:22 +0200

    Added CSharp OOP course solutions

---
+++ CSharp-OOP/04.1.Polymorphism-Lab/Animals/Animals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        protected Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavoriteFood = favouriteFood;
+        }
+
+        public string Name { get; }
+
+        public string FavoriteFood { get; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavoriteFood}";
+        }
+    }
+}
---
+++ CSharp-OOP/04.1.Polymorphism-Lab/MathOperation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        public static void Main()
+        {            
+            int intResult = MathOperations.Add(2, 3);
+            double doubleResult = MathOperations.Add(2.2, 3.3, 5.5);
+            decimal decimalResult = MathOperations.Add(2.2m, 3.3m, 4.4m);
+
+            Console.WriteLine(intResult);
+            Console.WriteLine(doubleResult);
+            Console.WriteLine(decimalResult);
+        }
+    }
+}
