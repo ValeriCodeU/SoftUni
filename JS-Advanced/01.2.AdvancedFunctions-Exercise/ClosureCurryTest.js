43337cb64b2b8512fefa11d0e4973ee8152d0c5b
ValeriCodeU <valeri.yanev@icloud.com>
2022-04-28 23:02:02 +0300

    Added JS Advanced course solutions

--- JS-Advanced/01.2.AdvancedFunctions-Exercise/ClosureCurryTest.js
+++
@@ -1,9 +0,0 @@
-function test(a){
-
-    return function (b){
-        return a + b;
-    }
-}
-let result = test(2)(3);
-
-console.log(result);
--- JS-Advanced/01.2.AdvancedFunctions-Exercise/RecursionClosureTest.js
+++
@@ -1,4 +0,0 @@
-function sum(num){
-    return num > 0 ? num + sum(num - 1) : 0;
-}
-console.log(sum(5));
