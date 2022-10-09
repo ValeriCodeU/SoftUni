31f8ac01858e74299277ee36028b6465f1f985cf
ValeriCodeU <valeri.yanev@icloud.com>
2022-10-09 22:55:25 +0300

    Entity Framework Core Exam

--- CSharpDatabasesAdvanced/DBEntityFrameworkExam/Trucks/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Trucks.Data
-{
-    public class Configuration
-    {
-        public static string ConnectionString = @"Server=DELLG3-PC\SQLEXPRESS;Database=Trucks;Trusted_Connection=True";
-    }
-}
