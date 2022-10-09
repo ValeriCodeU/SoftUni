80ddf1cea29f0168d49012e6dd383c60554ffc79
ValeriCodeU <valeri.yanev@icloud.com>
2022-10-09 23:01:08 +0300

    Removed connection string to local db

--- CSharp-Databases-Advanced/DBEntityFrameworkExam/Trucks/Data/Configuration.cs
+++ CSharp-Databases-Advanced/DBEntityFrameworkExam/Trucks/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public class Configuration
     {
-        public static string ConnectionString = @"Server=DELLG3-PC\SQLEXPRESS;Database=Trucks;Trusted_Connection=True";
+        public static string ConnectionString = "";
     }
 }
