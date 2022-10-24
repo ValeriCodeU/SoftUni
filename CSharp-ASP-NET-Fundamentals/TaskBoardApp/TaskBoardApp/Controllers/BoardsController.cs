942a429ef61308ed0b59e0c63bac6db9d5a845fd
ValeriCodeU <valeri.yanev@icloud.com>
2022-10-24 23:25:46 +0300

    Created the MVC structure of the app

---
+++ CSharp-ASP-NET-Fundamentals/TaskBoardApp/TaskBoardApp/Controllers/BoardsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskBoardApp.Data;
+using TaskBoardApp.Models;
+
+namespace TaskBoardApp.Controllers
+{
+	public class BoardsController : Controller
+	{
+		private readonly TaskBoardAppDbContext dbContext;
+
+		public BoardsController(TaskBoardAppDbContext _dbContext)
+		{
+			dbContext = _dbContext;
+		}
+
+		public async Task<IActionResult> All()
+		{
+			var model = dbContext.Boards
+				.Select(b => new BoardViewModel()
+				{
+					Id = b.Id,
+					Name = b.Name,
+					Tasks = b.Tasks.Select(t => new TaskViewModel()
+					{
+						Id = t.Id,
+						Title = t.Title,
+						Description = t.Description,
+						Owner = t.Owner.UserName
+					})
+				}).ToListAsync();
+
+			return View(model);
+		}
+	}
+}
---
+++ CSharp-ASP-NET-Fundamentals/TaskBoardApp/TaskBoardApp/Models/BoardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TaskBoardApp.Data.DataConstants.Board;
+
+namespace TaskBoardApp.Models
+{
+	public class BoardViewModel
+	{
+		public int Id { get; init; }
+
+        public string Name { get; init; }
+
+        public IEnumerable<TaskViewModel> Tasks { get; set; } = new List<TaskViewModel>();
+    }
+}
