bb6d5097cbafc23590eeb03530d44359446e0471
ValeriCodeU <valeri.yanev@icloud.com>
2022-10-30 23:45:55 +0200

    Created HttpGet action and view for editing a task

--- CSharp-ASP-NET-Fundamentals/TaskBoardApp/TaskBoardApp/Controllers/TasksController.cs
+++ CSharp-ASP-NET-Fundamentals/TaskBoardApp/TaskBoardApp/Controllers/TasksController.cs
@@ -14,9 +14,10 @@ namespace TaskBoardApp.Controllers
 
         private readonly ITaskService taskService;  
 
-        public TasksController(ITaskService _taskService)
+        public TasksController(ITaskService _taskService, TaskBoardAppDbContext _dbContext)
         {
             taskService = _taskService;
+            dbContext = _dbContext;
         }
 
         public async Task <IActionResult> Create()
@@ -69,6 +70,35 @@ namespace TaskBoardApp.Controllers
 
             return View(model);
         }
-        
+
+        [HttpGet]
+
+        public async Task<IActionResult> Edit (int id)
+        {
+            var task = await dbContext.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return BadRequest();
+            }
+
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            if (task.OwnerId != userId)
+            {
+                return Unauthorized();
+            }
+
+            var model = new TaskFormModel()
+            {
+                Title = task.Title,
+                Description = task.Description,
+                BoardId = task.BoardId,
+                Boards = await taskService.GetTaskBoardsAsync()
+            };
+
+            return View(model);
+        }
+
     }
 }
