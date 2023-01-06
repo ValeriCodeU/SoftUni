0f610171d22e1191bff6ffe6107ea859d4abe1ac
ValeriCodeU <valeri.yanev@icloud.com>
2023-01-06 21:16:48 +0200

    Added CSharp Data Structures course solutions

---
+++ CSharp-DataStructures-Fundamentals/04.Trees-Representation-and-Traversal-(BFS-DFS)/Tree/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //var tree = new Tree<int>(34, new Tree<int>(33));
+
+            //var tree = new Tree<int>(7,
+            //           new Tree<int>(19,
+            //                   new Tree<int>(1),
+            //                   new Tree<int>(12),
+            //                   new Tree<int>(31)),
+            //           new Tree<int>(21),
+            //           new Tree<int>(14,
+            //                   new Tree<int>(23),
+            //                   new Tree<int>(6)));
+
+            //tree.AddChild(19, new Tree<int>(40));
+            //tree.RemoveNode(19);
+
+            //лектор
+
+            var tree = new Tree<int>(34,
+                
+                new Tree<int>(36, 
+                        new Tree<int>(42), 
+                        new Tree<int>(3, 
+                                new Tree<int>(78))), 
+                new Tree<int>(1, 
+                        new Tree<int>(6)), 
+                new Tree<int>(103));
+
+
+            Console.WriteLine(String.Join(", ", tree.OrderBfs()));
+            Console.WriteLine(String.Join(", ", tree.OrderDfs()));
+            
+        }
+    }
+}
---
+++ CSharp-DataStructures-Fundamentals/04.Trees-Representation-and-Traversal-(BFS-DFS)/Tree/Tree.cs
@@ -0,0 +1,178 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private T value;
+        private List<Tree<T>> children;
+        private Tree<T> parent;
+
+        public Tree(T _value)
+        {
+            value = _value;
+            children = new List<Tree<T>>();
+        }
+
+        public Tree(T value, params Tree<T>[] children)
+            : this(value)
+        {
+            foreach (var child in children)
+            {
+                child.parent = this;
+                this.children.Add(child);
+            }
+        }
+
+        public void AddChild(T parentKey, Tree<T> child)
+        {
+            var parentNode = FindNodeWithBfs(parentKey);
+
+            if (parentNode != null)
+            {
+                parentNode.children.Add(child);
+                child.parent = parentNode;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }       
+
+        public IEnumerable<T> OrderBfs()
+        {
+            var queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            var result = new List<T>();
+
+            while (queue.Any())
+            {
+                var node = queue.Dequeue();
+                result.Add(node.value);
+
+                foreach (var child in node.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+
+        public IEnumerable<T> OrderDfs()
+        {
+            var result = new List<T>();
+            this.DfsWithRecursion(this, result);
+
+            //var result = this.DfsWithStack();
+
+            return result;
+        }
+
+        public void RemoveNode(T nodeKey)
+        {
+            var toBeDeletedNode = FindNodeWithBfs(nodeKey);
+
+            if (toBeDeletedNode == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var parentNode = toBeDeletedNode.parent;
+
+            if (parentNode == null)
+            {
+                throw new ArgumentException();
+            }
+
+            parentNode.children.Remove(toBeDeletedNode);
+        }
+
+        public void Swap(T firstKey, T secondKey)
+        {
+            var firstNode = FindNodeWithBfs(firstKey);
+            var secondNode = FindNodeWithBfs(secondKey);
+
+            if (firstNode == null || secondNode == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var firstParentNode = firstNode.parent;
+            var secondParentNode = secondNode.parent;
+
+            if (firstParentNode == null || secondParentNode == null)
+            {
+                throw new ArgumentException();
+            }
+
+            var indexOfFirstChild = firstParentNode.children.IndexOf(firstNode);
+            var indexOfSecondChild = secondParentNode.children.IndexOf(secondNode);
+
+            firstParentNode.children[indexOfFirstChild] = secondNode;
+            secondNode.parent = firstParentNode;
+
+            secondParentNode.children[indexOfSecondChild] = firstNode;
+            firstNode.parent = secondParentNode;
+        }
+
+        private Tree<T> FindNodeWithBfs(T nodeKey)
+        {
+            var queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Any())
+            {
+                var node = queue.Dequeue();
+
+                if (node.value.Equals(nodeKey))
+                {
+                    return node;
+                }
+
+                foreach (var child in node.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+
+        private IEnumerable<T> DfsWithStack()
+        {
+            var result = new Stack<T>();
+
+            var stack = new Stack<Tree<T>>();
+
+            stack.Push(this);
+
+            while (stack.Any())
+            {
+                var node = stack.Pop();
+                foreach (var child in node.children)
+                {
+                    stack.Push(child);
+                }
+
+                result.Push(node.value);
+            }
+
+            return result;
+
+        }
+
+        private void DfsWithRecursion(Tree<T> tree, List<T> result)
+        {
+            foreach (var child in tree.children)
+            {
+                this.DfsWithRecursion(child, result);
+            }
+            
+            result.Add(tree.value);
+        }
+    }
+}
