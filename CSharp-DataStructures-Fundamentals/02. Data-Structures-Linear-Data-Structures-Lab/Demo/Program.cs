5ff9302f159794352e3c157b5d3a6785db69c182
ValeriCodeU <valeri.yanev@icloud.com>
2023-05-27 22:02:06 +0300

    Problem 4 is solved

--- CSharp-DataStructures-Fundamentals/02. Data-Structures-Linear-Data-Structures-Lab/Demo/Program.cs
+++ CSharp-DataStructures-Fundamentals/02. Data-Structures-Linear-Data-Structures-Lab/Demo/Program.cs
@@ -29,13 +29,47 @@
 
 //Problem 3. Queue
 
-var queue = new Problem03.Queue.Queue<int>();
+//var queue = new Problem03.Queue.Queue<int>();
 
-queue.Enqueue(1);
-queue.Enqueue(2);
-queue.Enqueue(3);
+//queue.Enqueue(1);
+//queue.Enqueue(2);
+//queue.Enqueue(3);
 
 
-Console.WriteLine(String.Join(" ", queue));
+//Console.WriteLine(String.Join(" ", queue));
 
-Console.WriteLine(queue.Dequeue());
+//Console.WriteLine(queue.Dequeue());
+
+//var linkedList = new Problem04.SinglyLinkedList.SinglyLinkedList<int>();
+
+//linkedList.AddFirst(1);
+//linkedList.AddFirst(2);
+//linkedList.AddFirst(3);
+
+
+//Console.WriteLine(String.Join(" ", linkedList));
+
+//var linkedList = new Problem04.SinglyLinkedList.SinglyLinkedList<int>();
+
+//linkedList.AddLast(1);
+//linkedList.AddLast(2);
+//linkedList.AddLast(3);
+
+
+//Console.WriteLine(String.Join(" ", linkedList));
+
+var linkedList = new Problem04.SinglyLinkedList.SinglyLinkedList<int>();
+
+linkedList.AddLast(1);
+linkedList.AddLast(2);
+linkedList.AddLast(3);
+linkedList.AddLast(4);
+linkedList.AddLast(5);
+linkedList.AddLast(6);
+
+linkedList.RemoveLast();
+linkedList.RemoveLast();
+
+Console.WriteLine(linkedList.Count);
+
+Console.WriteLine(String.Join(" ", linkedList));
--- CSharp-DataStructures-Fundamentals/02. Data-Structures-Linear-Data-Structures-Lab/Problem04.SinglyLinkedList/SinglyLinkedList.cs
+++ CSharp-DataStructures-Fundamentals/02. Data-Structures-Linear-Data-Structures-Lab/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -6,46 +6,150 @@
 
     public class SinglyLinkedList<T> : IAbstractLinkedList<T>
     {
-        public int Count => throw new NotImplementedException();
+        private class Node
+        {
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+            {
+                this.Element = element;
+            }
+        }
+
+        private Node head;
+
+        public int Count { get; set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(item, this.head);
+            this.head = newNode;
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(item);
+
+            if (this.Count == 0)
+            {
+                this.head = newNode;
+            }
+
+            else
+            {
+                var currentNode = this.head;
+
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                currentNode.Next = newNode;
+            }
+
+            this.Count++;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                yield return currentNode.Element;
+                currentNode = currentNode.Next;
+            }
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return this.head.Element;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var currentNode = this.head;
+
+            while (currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            return currentNode.Element;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var firstNode = this.head;
+            this.head = firstNode.Next;
+            this.Count--;
+
+            return firstNode.Element;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {                
+                throw new InvalidOperationException();
+            }
+
+            var currentNode = this.head;
+            T result = default;
+            var count = this.Count - 1;            
+
+            if (this.Count == 1)
+            {
+                result = currentNode.Element;
+                currentNode = null;                
+            }
+            else
+            {               
+
+                while (count > 0)
+                {
+                    count--;
+
+                    if (count == 0)
+                    {
+                        result = currentNode.Next.Element;
+                        currentNode.Next = null;
+                    }
+
+                    currentNode = currentNode.Next;
+                }
+            }
+
+            this.Count--;
+
+            return result;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return this.GetEnumerator();
         }
     }
 }
