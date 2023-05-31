0ec418a8f27cffe8b848f881619796c5c02075c4
ValeriCodeU <valeri.yanev@icloud.com>
2023-05-31 17:19:52 +0300

    Problem 1 is solved

--- CSharp-DataStructures-Fundamentals/03. Linear-Data-Structures-Exercise/01.FasterQueue/CircularQueue.cs
+++ CSharp-DataStructures-Fundamentals/03. Linear-Data-Structures-Exercise/01.FasterQueue/CircularQueue.cs
@@ -6,36 +6,90 @@
 
     public class CircularQueue<T> : IAbstractQueue<T>
     {
-        public int Count => throw new NotImplementedException();
+        private T[] elements;
+        private int startIndex;
+        private int endIndex;
+
+        public CircularQueue(int capacity = 4)
+        {
+            this.elements = new T[capacity];
+        }
+
+        public int Count { get; set; }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var result = this.elements[startIndex];
+            this.startIndex = (startIndex + 1) % this.elements.Length;
+            this.Count--;
+
+            return result;
+        }
+
+        private void Grow()
+        {
+            this.elements = this.CopyElements(new T[this.elements.Length * 2]);
+            this.startIndex = 0;
+            this.endIndex = this.Count;
+        }
+
+        private T[] CopyElements(T[] resultArray)
+        {
+            int sourceIndex = this.startIndex;
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                resultArray[i] = this.elements[sourceIndex];
+                sourceIndex = (sourceIndex + 1) % this.elements.Length;
+            }
+
+            return resultArray;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
-        }
+            if (this.Count >= this.elements.Length)
+            {
+                this.Grow();
+            }
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
+            this.elements[endIndex] = item;
+            this.endIndex = (this.endIndex + 1) % this.elements.Length;
+            this.Count++;
+
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.elements[startIndex];
         }
 
         public T[] ToArray()
         {
-            throw new NotImplementedException();
+            return this.CopyElements(new T[this.Count]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return this.elements[(this.startIndex + i) % this.elements.Length];
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return this.GetEnumerator();
         }
     }
 
---
+++ CSharp-DataStructures-Fundamentals/03. Linear-Data-Structures-Exercise/Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using Problem01.CircularQueue;
+
+var queue = new CircularQueue<int>();
+
+queue.Enqueue(1);
+queue.Enqueue(2);
+queue.Enqueue(3);
+queue.Enqueue(4);
+queue.Enqueue(5);
+queue.Dequeue();
+queue.Dequeue();
+queue.Dequeue();
+
+Console.WriteLine(String.Join(", ", queue));
+Console.WriteLine(queue.Peek());
