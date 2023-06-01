fc2cd4932e402987051ff05d9dac0135f276463a
ValeriCodeU <valeri.yanev@icloud.com>
2023-06-01 15:44:46 +0300

    Problem 2 is solved

--- CSharp-DataStructures-Fundamentals/03. Linear-Data-Structures-Exercise/02.DoublyLinkedList/DoublyLinkedList.cs
+++ CSharp-DataStructures-Fundamentals/03. Linear-Data-Structures-Exercise/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -6,46 +6,146 @@
 
     public class DoublyLinkedList<T> : IAbstractLinkedList<T>
     {
-        public int Count { get; private set; }
+        private class Node
+        {
+            public Node Next { get; set; }
+            public Node Previous { get; set; }
+            public T Element { get; set; }
+
+            public Node(T element)
+            {
+                this.Element = element;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+
+        public int Count { get; set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(item);
+
+            if (this.head == null)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                newNode.Next = this.head;
+                this.head.Previous = newNode;
+                this.head = newNode;
+            }
+
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(item);
+
+            if (this.Count == 0)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = this.tail;
+                this.tail.Next = newNode;
+                this.tail = newNode;
+            }
+
+            this.Count++;
+           
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            if (this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.head.Element;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            if (this.tail == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.tail.Element;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            if (this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var resultElement = this.head.Element;
+
+            if (this.Count == 1)
+            {
+                this.head = null;
+                this.tail = null;
+            }
+            else
+            {
+                this.head = this.head.Next;
+                this.head.Previous = null;
+            }
+            
+            this.Count--;
+
+            return resultElement;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            if (this.tail == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var resultElement = this.tail.Element;
+
+            if (this.Count == 1)
+            {
+                this.head = null;
+                this.tail = null;
+            }
+            else
+            {
+                this.tail = this.tail.Previous;
+                this.tail.Next = null;
+            }
+
+            this.Count--;
+
+            return resultElement;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.head;
+
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return this.GetEnumerator();
         }
     }
 }
