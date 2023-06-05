5e2adb6546e896e3e99dd5a77f53aba1058e3f7f
ValeriCodeU <valeri.yanev@icloud.com>
2023-06-05 13:41:36 +0300

    Problem 4 is solved

--- CSharp-DataStructures-Fundamentals/03. Linear-Data-Structures-Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
+++ CSharp-DataStructures-Fundamentals/03. Linear-Data-Structures-Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -7,7 +7,53 @@
     {
         public bool AreBalanced(string parentheses)
         {
-            throw new NotImplementedException();
+
+            if (parentheses.Length % 2 == 1 || String.IsNullOrWhiteSpace(parentheses))
+            {
+                return false;
+            }
+
+            var stack = new Stack<char>(parentheses.Length / 2);
+
+            for (int i = 0; i < parentheses.Length; i++)
+            {
+                var currentChar = parentheses[i];
+                char expectedChar = default;
+
+                if (currentChar == ')')
+                {
+                    expectedChar = '(';
+                }
+                else if (currentChar == ']')
+                {
+                    expectedChar = '[';
+
+                }
+                else if (currentChar == '}')
+                {
+                    expectedChar = '{';
+
+                }
+                else
+                {
+                    stack.Push(currentChar);
+                }
+
+
+
+                if (expectedChar == default)
+                {
+                    continue;
+                }
+
+                if (stack.Pop() != expectedChar)
+                {
+                    return false;
+                }
+
+            }
+
+            return stack.Count == 0;
         }
     }
 }
--- CSharp-DataStructures-Fundamentals/03. Linear-Data-Structures-Exercise/Demo/Program.cs
+++ CSharp-DataStructures-Fundamentals/03. Linear-Data-Structures-Exercise/Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Problem01.CircularQueue;
 using Problem03.ReversedList;
+using Problem04.BalancedParentheses;
 
 //var queue = new CircularQueue<int>();
 
@@ -15,16 +16,25 @@ using Problem03.ReversedList;
 //Console.WriteLine(String.Join(", ", queue));
 //Console.WriteLine(queue.Peek());
 
-var list = new ReversedList<int>();
+//var list = new ReversedList<int>();
 
-list.Add(10);
-list.Add(11);
-list.Add(2);
-list.Add(4);
-list.Add(15);
+//list.Add(10);
+//list.Add(11);
+//list.Add(2);
+//list.Add(4);
+//list.Add(15);
 
-Console.WriteLine(String.Join(", ", list));
+//Console.WriteLine(String.Join(", ", list));
 
-list.Insert(1, 3);
+//list.Insert(1, 3);
 
-Console.WriteLine(String.Join(", ", list));
+//Console.WriteLine(String.Join(", ", list));
+
+
+var balancedParentheses = new BalancedParenthesesSolve();
+
+Console.WriteLine(balancedParentheses.AreBalanced("{[()]}"));
+//Console.WriteLine(balancedParentheses.AreBalanced("{[(]]}"));
+//Console.WriteLine(balancedParentheses.AreBalanced("()()()()()()()()()()"));
+//Console.WriteLine(balancedParentheses.AreBalanced("()[]{}{}}"));
+//Console.WriteLine(balancedParentheses.AreBalanced("((((((())))))))))))))))"));
